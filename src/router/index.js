71ccc5b8767a55614481e046f6d298873ba4af5d
zhaoye <zhaoye@fltrp.com>
2018-07-30 10:38:15 +0800

    vue-router中异步加载组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../pages/home/home'
-import City from '../pages/city/city'
-import Detail from '../pages/detail/detail'
 
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
+    // 使用异步组件加载以提高性能
     routes: [
         {
             path: '/',
-            component: Home,
+            component: () => import('../pages/home/home'),
             name: 'Home'
         },
         {
             path: '/city',
-            component: City,
+            component: () => import('../pages/city/city'),
             name: 'City'
         },
         {
             path: '/detail/:id',
-            component: Detail,
+            component: () => import('../pages/detail/detail'),
             name: 'Detail'
         }
     ],
